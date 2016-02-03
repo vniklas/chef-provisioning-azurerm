72d65fd203baea65b447cc834390955be613a832
vNiklas Akerlund <niklas@djungeln.se>
2016-02-03 21:58:57 +0100

    updated

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,18 @@
 require 'chef/provisioning/azurerm'
 with_driver 'AzureRM:90cb812a-eb9b-4a1e-8fd7-75e6d367f760'
 
-azure_resource_group 'nic-demo22' do
+azure_resource_group 'nics2-demo22' do
   location 'West US' # optional, default: 'West US'
   tags businessUnit: 'NIC' # optional
 end
 
 azure_resource_template 'nic-deployment' do
-  resource_group 'nic-demo22'
+  resource_group 'nics2-demo22'
   template_source 'azure_deploy.json'
   parameters adminUsername: 'mfleksnes',
              adminPassword: 'P2ssw0rd',
              windowsOSVersion: '2012-R2-Datacenter',
-             dnsLabelPrefix: 'vniklasnic22'
+             dnsLabelPrefix: 'nics2-demo22'
   chef_extension client_type: 'ChefClient',
                  version: '1210.12',
                  client_rb: '',
